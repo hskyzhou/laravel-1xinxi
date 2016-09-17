7e6a577dd9f6d19efb91236297c7b258701d1db6
hsky <xezw211@126.com>
2016-09-18 00:15:38 +0800

    修改

--- src/XinXi.php
+++ src/XinXi.php
@@ -10,5 +10,18 @@
 		 * @date		2016-09-16 07:24:54
 		 * @return		
 		 */
-		
+		public function sendNormalInfo(){
+
+		}
+
+		/**
+		 * 发送 自定义短信
+		 * @param		
+		 * @author		xezw211@12.com
+		 * @date		2016-09-16 07:24:54
+		 * @return		
+		 */
+		public function sendCustomInfo(){
+			
+		}
 	}
